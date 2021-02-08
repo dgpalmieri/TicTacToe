770ff420a95947f7a082c8a9d0933bab50f63dd6
Dylan Palmieri <dgpalmieri@gmail.com>
2021-02-08 14:49:43 -0900

    Add support for saving Add support for draw into win checker

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -4,7 +4,7 @@
 # Main game functions for 5x5 Tic-Tac-Toe
 
 import ai
-from time import localtime, strftime
+from time import localtime, strftime, sleep
 
 
 def print_board(board):
@@ -66,20 +66,41 @@ def check_win(board):
                 board[i] == board[i+5] == board[i+10] == board[i+15]):
             return board[i]
 
+    # Possible draw
+    for char in board:
+        if (char == ' '):
+            continue
+        return "draw"
+
     return ' '
 
 
+def save_game(path, board_list):
+    if (path == ""):
+        path = "tictactoe_"
+        path = path + strftime("%Y-%m-%d_%H:%M:%S", localtime())
+
+    with open(path, "a") as fout:
+        for elem in board_list:
+            fout.write(''.join(elem))
+            fout.write("\n")
+    print("Your game has been saved as {}".format(path))
+
+
 def game(board, path=""):
-    print("Would you like to play against:")
-    print("[1] A human player.")
-    print("[2] An AI player.")
-    print("[3] I'd like to watch two AI players play.")
-    print("[4] Exit the game.")
-    player = input("Please choose either 1, 2, 3, or 4: ")
-    if (player != '1' or player != '2' or player != '3'):
-        if (player == '4'):
-            return -1
-        print("Please choose again.")
+    while(True):
+        print("Would you like to play against:")
+        print("[1] A human player.")
+        print("[2] An AI player.")
+        print("[3] I'd like to watch two AI players play.")
+        print("[4] Exit the game.")
+        player = input("Please choose either 1, 2, 3, or 4: ")
+        if (player != "1" and player != "2" and player != "3"):
+            if (player == "4"):
+                return -1
+            print("Please choose again.")
+            continue
+        break
 
     board_list = []
 
@@ -95,16 +116,13 @@ def game(board, path=""):
             elif (char == 'O'):
                 turn -= 1
 
-        if (turn == 1):
+        if (turn == 0):
             turn = 'X'
         elif (turn == -1):
             turn = 'O'
 
-        print("This is the current board state:")
-        print_board(board)
-
-        print("Based on the state of the board, I can see that it is \
-            {}'s turn.".format(turn))
+        print("Based on the state of the board, I can see that", end="")
+        print("it is {}'s turn.".format(turn))
 
         if (turn == 'O'):
             if (player == '2'):
@@ -114,9 +132,9 @@ def game(board, path=""):
                 while(True):
                     print("\'O\' player's move:")
                     print("Where would you like to place your \'O\'? ")
-                    print("Format your move as a number between 1 and 25, \
-                        with 1 being the top left square and 25 being the \
-                        bottom right.")
+                    print("Format your move as a number between 1 ", end="")
+                    print("and 25, with 1 being the top left square ", end="")
+                    print("and 25 being the bottom right.")
                     move = input("Please enter your move: ")
                     move = int(move)
 
@@ -132,24 +150,17 @@ def game(board, path=""):
     turn = 'X'
 
     while(True):
-        board_list.append(board)
+        board_list.append(board.copy())
         print("This is the current board state:")
         print_board(board)
         print("It is \'{}\' player's move:".format(turn))
-        if (turn == 'O' and player == '2' or player == '3'):
-            ai.AI_move(board)
+        if ((turn == 'O' and player == '2') or player == '3'):
+            ai.AI_move(board, turn)
             sleep(1)
         else:
             save = input("Would you like to save the game and quit? y/n ")
             if (save.lower() == 'y'):
-                if (path == ""):
-                    path = "tictactoe "
-                    path.append(strftime("%Y-%m-%d %H:%M:%S", localtime()))
-
-                with open(path, "a") as fout:
-                    for elem in board_list:
-                        fout.write(board)
-                print("Your game has been saved as {}".format(path))
+                save_game(path, board_list)
                 return 2
             elif (save.lower() == 'n'):
                 pass
@@ -170,8 +181,18 @@ def game(board, path=""):
             turn = 'O'
         else:
             turn = 'X'
+
         winner = check_win(board)
-        if (winner != ' '):
+        if (winner == 'X' or winner == 'O'):
             print("We have a winner! \'{}\' wins!".format(winner))
             print_board(board)
-            return 1
+        elif (winner == "draw"):
+            print("It's a draw!")
+        else:
+            continue
+
+        save = input("Would you like to save the game? y/n ")
+        if (save.lower() == 'y'):
+            save_game(path, board_list)
+        return
+
