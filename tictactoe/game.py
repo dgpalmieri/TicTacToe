8a587b581c1a8e173ba227be83c8da991317cb0c
Dylan Palmieri <dgpalmieri@gmail.com>
2021-02-08 04:12:55 -0900

    Add gameplay loop

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -3,6 +3,9 @@
 # 2021-02-07
 # Main game functions for 5x5 Tic-Tac-Toe
 
+import ai
+
+
 def print_board(board):
     if (len(board) < 25):
         print("Something's wrong with the board. len() < 25")
@@ -63,3 +66,96 @@ def check_win(board):
             return board[i]
 
     return ' '
+
+
+def game(board):
+    print("Would you like to play against:")
+    print("[1] A human player.")
+    print("[2] An AI player.")
+    print("[3] I'd like to watch two AI players play.")
+    print("[4] Exit the game.")
+    player = input("Please choose either 1, 2, 3, or 4: ")
+    if (player != '1' or player != '2' or player != '3'):
+        if (player == '4'):
+            return -1
+        print("Please choose again.")
+
+    empty_board = []
+    for _ in range(25):
+        empty_board.append(" ")
+
+    if (board != empty_board):
+        turn = 0
+        for char in board:
+            if (char == 'X'):
+                turn += 1
+            elif (char == 'O'):
+                turn -= 1
+
+        if (turn == 1):
+            turn = 'X'
+        elif (turn == -1):
+            turn = 'O'
+
+        print("This is the current board state:")
+        print_board(board)
+
+        print("Based on the state of the board, I can see that it is \
+            {}'s turn.".format(turn))
+
+        if (turn == 'O'):
+            if (player == '2'):
+                print("AI move:")
+                ai.AI_move(board)
+            else:
+                while(True):
+                    print("\'O\' player's move:")
+                    print("Where would you like to place your \'O\'? ")
+                    print("Format your move as a number between 1 and 25, \
+                        with 1 being the top left square and 25 being the \
+                        bottom right.")
+                    move = input("Please enter your move: ")
+                    move = int(move)
+
+                    if (move > 0 and move < 26 and board[move] != 'X' and
+                            board[move] != 'O'):
+                        move = int(move)-1
+                        board[move] = 'O'
+                        break
+                    else:
+                        print("Please enter a valid move.")
+                        continue
+
+    turn = 'X'
+
+    while(True):
+        print("This is the current board state:")
+        print_board(board)
+        print("It is \'{}\' player's move:".format(turn))
+        if (turn == 'O' and player == '2'):
+            ai.AI_move(board)
+            sleep(1)
+        elif (player == '3'):
+            ai.AI_move(board)
+            sleep(1)
+        else:
+            print("Where would you like to place your \'{}\'?".format(turn))
+            print("Format your move as a number between 1 and 25, ", end="")
+            print("with 1 being the top left square and 25 the bottom right.")
+            move = input("Please enter your move: ")
+            move = int(move)-1
+            if (move >= 0 and move <= 24 and board[move] != 'X' and
+                    board[move] != 'O'):
+                board[move] = turn
+            else:
+                print("Please enter a valid move.")
+                continue
+        if (turn == 'X'):
+            turn = 'O'
+        else:
+            turn = 'X'
+        winner = check_win(board)
+        if (winner != ' '):
+            print("We have a winner! \'{}\' wins!".format(winner))
+            print_board(board)
+            return 1
