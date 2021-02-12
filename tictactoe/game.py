f12bedde417458e65caa65b914a7fa1609bf5da4
Dylan Palmieri <dgpalmieri@gmail.com>
2021-02-11 18:26:18 -0900

    Refactor to improve readability

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -1,69 +1,85 @@
-# game.py
-# Dylan Palmieri
-# 2021-02-07
-# Main game functions for 5x5 Tic-Tac-Toe
+"""
+game.py
+Dylan Palmieri
+2021-02-07
+Main game functions for 5x5 Tic-Tac-Toe
 
+This module contains the game code for the tictactoe module.
+
+Included is the print_board method, the check_win method, the
+save_game method, and the game method.
+"""
+
+
+from time import localtime, strftime
 import ai
-from time import localtime, strftime, sleep
 
 
 def print_board(board):
+    """
+    This method, given a board, prints the board in the standard
+    tic-tac-toe board format.
+    """
     if len(board) < 25:
         print("Something's wrong with the board. len() < 25")
         return
 
     print()
     print()
-    print("            |      |      |      |      ")
-    print("        {}   |  {}   |  {}   |  {}   |  {}   ".format(
+    print("             |       |       |       |      ")
+    print("         {}   |   {}   |   {}   |   {}   |   {}   ".format(
         board[0], board[1], board[2], board[3], board[4]))
-    print("      ______|______|______|______|______")
-    print("            |      |      |      |      ")
-    print("        {}   |  {}   |  {}   |  {}   |  {}   ".format(
+    print("      _______|_______|_______|_______|_______")
+    print("             |       |       |       |      ")
+    print("         {}   |   {}   |   {}   |   {}   |   {}   ".format(
         board[5], board[6], board[7], board[8], board[9]))
-    print("      ______|______|______|______|______")
-    print("            |      |      |      |      ")
-    print("        {}   |  {}   |  {}   |  {}   |  {}   ".format(
+    print("      _______|_______|_______|_______|_______")
+    print("             |       |       |       |      ")
+    print("         {}   |   {}   |   {}   |   {}   |   {}   ".format(
         board[10], board[11], board[12], board[13], board[14]))
-    print("      ______|______|______|______|______")
-    print("            |      |      |      |      ")
-    print("        {}   |  {}   |  {}   |  {}   |  {}   ".format(
+    print("      _______|_______|_______|_______|_______")
+    print("             |       |       |       |      ")
+    print("         {}   |   {}   |   {}   |   {}   |   {}   ".format(
         board[15], board[16], board[17], board[18], board[19]))
-    print("      ______|______|______|______|______")
-    print("            |      |      |      |      ")
-    print("        {}   |  {}   |  {}   |  {}   |  {}   ".format(
+    print("      _______|_______|_______|_______|_______")
+    print("             |       |       |       |      ")
+    print("         {}   |   {}   |   {}   |   {}   |   {}   ".format(
         board[20], board[21], board[22], board[23], board[24]))
-    print("            |      |      |      |      ")
+    print("             |       |       |       |      ")
     print()
     print()
 
 
 def check_win(board):
+    """
+    This function checks for all possible four-in-a-row wins on a
+    5x5 tic-tac-toe board.
+    """
     # Possible Horizontal Wins
     horizontal = [0, 1, 5, 6, 10, 11, 15, 16, 20, 21]
     for i in horizontal:
-        if (board[i] != ' ' and
-                board[i] == board[i+1] == board[i+2] == board[i+3]):
+        if (board[i] != ' '
+           and board[i] == board[i + 1] == board[i + 2] == board[i + 3]):
             return board[i]
 
     # Possible Diagonal wins, left to right
     l_diagonal = [0, 1, 5, 6]
     for i in l_diagonal:
-        if (board[i] != ' ' and
-                board[i] == board[i+5+1] == board[i+10+2] == board[i+15+3]):
+        if (board[i] != ' '
+           and board[i] == board[i + 5 + 1] == board[i + 10 + 2] == board[i + 15 + 3]):
             return board[i]
 
     # Possible Diagonal wins, right to left
     r_diagonal = [3, 4, 8, 9]
     for i in r_diagonal:
-        if (board[i] != ' ' and
-                board[i] == board[i+5-1] == board[i+10-2] == board[i+15-3]):
+        if (board[i] != ' '
+           and board[i] == board[i + 5 - 1] == board[i + 10 - 2] == board[i + 15 - 3]):
             return board[i]
 
     # Possible Vertical wins
     for i in range(10):
-        if (board[i] != ' ' and
-                board[i] == board[i+5] == board[i+10] == board[i+15]):
+        if (board[i] != ' '
+           and board[i] == board[i + 5] == board[i + 10] == board[i + 15]):
             return board[i]
 
     # Possible draw
@@ -79,7 +95,10 @@ def check_win(board):
 
 
 def save_game(path, board_list):
-    if (path == ""):
+    """
+    This function saves a list of board states to a file.
+    """
+    if path == "":
         path = "tictactoe_"
         path = path + strftime("%Y-%m-%d_%H:%M:%S", localtime())
 
@@ -90,81 +109,39 @@ def save_game(path, board_list):
     print("Your game has been saved as {}".format(path))
 
 
-def game(board, path=""):
-    while(True):
-        print("Would you like to play against:")
-        print("[1] A human player.")
-        print("[2] An AI player.")
-        print("[3] I'd like to watch two AI players play.")
-        print("[4] Exit the game.")
-        player = input("Please choose either 1, 2, 3, or 4: ")
-        if player != "1" and player != "2" and player != "3":
-            if player == "4":
-                return -1
-            print("Please choose again.")
-            continue
-        break
-
+def game(board, path, player):
+    """
+    This function contains the main game loop for the tictactoe module.
+    TODO: clean up this function
+    """
     board_list = []
 
     empty_board = []
     for _ in range(25):
         empty_board.append(" ")
 
-    if board != empty_board:
-        turn = 0
-        for char in board:
-            if char == 'X':
-                turn += 1
-            elif char == 'O':
-                turn -= 1
-
-        if turn == 0:
-            turn = 'X'
-        elif turn == -1:
-            turn = 'O'
-
-        print("Based on the state of the board, I can see that ", end="")
-        print("it is {}'s turn.".format(turn))
-
-        if turn == 'O':
-            if player == '2':
-                print("AI move:")
-                ai.AI_move(board)
-            else:
-                while(True):
-                    print("\'O\' player's move:")
-                    print("Where would you like to place your \'O\'? ")
-                    print("Format your move as a number between 1 ", end="")
-                    print("and 25, with 1 being the top left square ", end="")
-                    print("and 25 being the bottom right. If you ", end="")
-                    print("would like to save and quit, please ", end="")
-                    print("enter \'wq\'")
-                    move = input("Please enter your move: ")
-                    if move == "wq":
-                        save_game(path, board_list)
-                        return 2
-                    move = int(move)
-
-                    if (move > 0 and move < 26 and board[move] != 'X' and
-                            board[move] != 'O'):
-                        move = int(move)-1
-                        board[move] = 'O'
-                        break
-                    else:
-                        print("Please enter a valid move.")
-                        continue
-
     turn = 'X'
 
-    while(True):
+    if board != empty_board:
+        num_x = board.count('X')
+        num_o = board.count('O')
+
+        if num_x == num_o:
+            turn = 'O'
+
+    print("Based on the state of the board, I can see that ", end="")
+    print("it is {}'s turn.".format(turn))
+
+    while True:
         board_list.append(board.copy())
+
         print("This is the current board state:")
         print_board(board)
         print("It is \'{}\' player's move:".format(turn))
+
         if (turn == 'O' and player == '2') or player == '3':
-            ai.AI_move(board, turn)
-            sleep(1)
+            ai.ai_move(board, turn)
+
         else:
             print("Where would you like to place your \'{}\'?".format(turn))
             print("Format your move as a number between 1 and 25, ", end="")
@@ -172,25 +149,26 @@ def game(board, path=""):
             print("would like to save and quit, please ", end="")
             print("enter \'wq\'")
             move = input("Please enter your move: ")
-            if (move == "wq"):
-                save_game(path, board_list)
-                return 2
 
-            move = int(move)-1
-            if (move >= 0 and move <= 24 and board[move] != 'X' and
-                    board[move] != 'O'):
+            if move == "wq":
+                save_game(path, board_list)
+                return
+
+            move = int(move) - 1
+            if (0 <= move <= 24 and board[move] not in ('X', 'O')):
                 board[move] = turn
             else:
                 print("Please enter a valid move.")
                 continue
+
         if turn == 'X':
             turn = 'O'
         else:
             turn = 'X'
 
         winner = check_win(board)
-        if winner == 'X' or winner == 'O':
-            print("We have a winner! \'{}\' wins!".format(winner))
+        if winner in ('X', 'O'):
+            print(f"We have a winner! \'{winner}\' wins!")
             print_board(board)
         elif winner == "draw":
             print("It's a draw!")
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,17 +1,29 @@
-# tictactoe.py
-# Dylan Palmieri
-# 2021-02-07
-# main function for 5x5 Tic-Tac-Toe
+"""
+tictactoe.py
+Dylan Palmieri
+2021-02-07
+main function for 5x5 Tic-Tac-Toe
 
-import ai
-import game as g
+This module contains the main UI loop for the tictactoe module.
+
+It holds the read_file, replay_game, and main functions.
+"""
+
+
 from time import sleep
+from game import game
+from game import print_board
 
 
 def read_file(board):
+    """
+    This function loads the last board state from a file,
+    specified by an absolute path.
+    """
     path = input("Please enter the absolute path of the game file: ")
     state = ""
     with open(path, "r") as fin:
+        line = ""
         for line in fin:
             pass
         state = line
@@ -23,6 +35,10 @@ def read_file(board):
 
 
 def replay_game():
+    """
+    This function reads a board state from a file, and calls the
+    game.print_board function to print the board state.
+    """
     path = input("Please enter the absolute path of the game file: ")
     board = []
     for _ in range(25):
@@ -31,55 +47,55 @@ def replay_game():
         for line in fin:
             for i in range(25):
                 board[i] = line[i]
-            g.print_board(board)
+            print_board(board)
             sleep(1)
-    return
 
 
 def main():
-    game = input("Hello. Would you like to play a game? y/n: ")
-    if (game.lower() == 'y'):
-        while(True):
+    """
+    The main function holds the main UI for the tictactoe module.
+    """
+    while True:
+        board = []
+        for _ in range(25):
+            board.append(' ')
 
-            print("At this juncture, you have several options:")
-            print("[1] Play a new game.")
-            print("[2] Continue a game (requires file input).")
-            print("[3] Watch a previously played game (requires file input).")
-            print("[4] Exit the game.")
-            option = input("Choose an option 1/2/3/4: ")
+        print("Welcome to 5x5 Tic-Tac-Toe!")
+        print("At this juncture, you have several options:")
+        print("[1] Play a new game.")
+        print("[2] Continue a game (requires file input).")
+        print("[3] Watch a previously played game (requires file input).")
+        print("[4] Exit the game.")
+        option = input("Choose an option 1/2/3/4: ")
 
-            board = []
-            for _ in range(25):
-                board.append(" ")
+        if option not in ('1', '2', '3', '4'):
+            option = input("Please choose a valid option,\
+                           either 1, 2, 3, or 4:")
+            continue
 
-            if (option == '1'):  # play a new game
-                exit = g.game(board)
-                if (exit == -1):
-                    break
-                if (exit == 1):
-                    continue
+        if option == '2':  # start a saved game
+            path = read_file(board)
 
-            elif (option == '2'):  # start a saved game
-                path = read_file(board)
-                exit = g.game(board, path)
-                if (exit == -1):
-                    break
-                if (exit == 1):
-                    continue
+        if option == '3':  # watch a previous game
+            replay_game()
+            print("End of File reached.")
+            continue
 
-            elif(option == '3'):  # watch a previous game
-                replay_game()
-                print("End of File reached.")
-                continue
-
-            elif (option == '4'):
+        print("Would you like to play against:")
+        print("[1] A human player.")
+        print("[2] An AI player.")
+        print("[3] I'd like to watch two AI players play.")
+        print("[4] Exit the game.")
+        player = input("Please choose either 1, 2, 3, or 4: ")
+        if player not in ('1', '2', '3'):
+            if player == "4":
                 break
+            print("Please choose again.")
+            continue
 
-            else:
-                option = input("Please choose a valid option,\
-                               either 1, 2, 3, or 4:")
+        game(board, path, player)
 
-        print("Goodbye.")
+    print("Goodbye.")
 
 
 if __name__ == '__main__':
