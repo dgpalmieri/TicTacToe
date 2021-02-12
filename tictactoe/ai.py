ff0511fec491b2e225474be60ea5ab8bc0ce336d
Dylan Palmieri <dgpalmieri@gmail.com>
2021-02-11 18:25:27 -0900

    Add alpha/beta pruning

--- tictactoe/ai.py
+++ tictactoe/ai.py
@@ -1,57 +1,81 @@
-# ai.py
-# Dylan Palmieri
-# 2021-02-08
-# AI functionality for 5x5 Tic-Tac-Toe
+"""
+ai.py
+Dylan Palmieri
+2021-02-08
+AI functionality for 5x5 Tic-Tac-Toe
 
-from random import randint
-from game import check_win
+This module contains the AI functionality for the tictactoe game.
+It holds the ai_move method, which is a wrapper for the minimax_ab
+method.
+"""
+
+
 from math import inf
+from random import randint
+import game
 
 
-def minimax(board, player_id, depth, maximize):
-    winner = check_win(board)
+def minimax_ab(board, player_id, depth, maximize, alpha=-inf, beta=-inf) -> (int, int):
+    """
+    # Minimax_ab
+
+    This function implements a minimax game tree search with alpha/beta
+    pruning.
+
+    It returns a tuple of (int, int), which represents the most optimal
+    move and that move's score, which is a sum of the different weights
+    of the boards.
+
+    The board weights are determined by the fastest possible win states.
+    """
+    winner = game.check_win(board)
     if winner == player_id:
         return None, 10 + depth
-    elif winner != player_id and winner != ' ' and winner != "draw":
+    if winner not in (' ', "draw"):
         return None, -10 - depth
-    elif winner == "draw":
+    if winner == "draw":
         return None, 0
-    elif depth == 0:
+    if depth == 0:
         return None, None
-    else:
-        score = None
-        if maximize:
-            score = -inf
-        else:
-            score = inf
 
-        best_move = None
+    best_move = None
+    value = None
 
-        for i in range(25):
-            if board[i] == ' ':
-                board[i] = player_id
-                _, value = minimax(board,
+    for i in range(25):
+        if board[i] == ' ':
+            board[i] = player_id
+            _, value = minimax_ab(board,
                                   'O' if player_id == 'X' else 'X',
                                   depth - 1,
-                                  False)
-                board[i] = ' '
-                if (value is not None and
-                    (value > score if maximize else value < score)):
-                    score = value
-                    best_move = i
+                                  False,
+                                  alpha,
+                                  beta)
+            board[i] = ' '
+            if (value is not None
+               and (value > alpha if maximize else value < beta)):
+                if maximize:
+                    alpha = value
+                else:
+                    beta = value
+                if (alpha != -inf and beta != inf
+                   and alpha >= beta if maximize else beta >= alpha):
+                    break
+                best_move = i
 
-        return best_move, score
+    if maximize:
+        return best_move, alpha
+
+    return best_move, beta
 
 
-def AI_move(board, player_id):
+def ai_move(board, player_id):
+    """
+    ai_move is a wrapper for the minimax_ab function.
+    """
 
-    move, _ = minimax(board, player_id, 5, True)
-    print("move: ", move)
+    move, _ = minimax_ab(board, player_id, 5, True)
 
-    if move == None:
-        move = randint(0,24)
-        print("random move")
+    if move is None:
+        move = randint(0, 24)
 
     board[move] = player_id
-
-    return
