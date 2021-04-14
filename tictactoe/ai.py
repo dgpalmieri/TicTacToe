1ea6e2786a52da3b879bb656b75292e541dcd09b
Dylan Palmieri <dgpalmieri@gmail.com>
2021-04-14 11:40:42 -0800

    Add Node class and remove minimax code

--- tictactoe/ai.py
+++ tictactoe/ai.py
@@ -5,7 +5,7 @@ Dylan Palmieri
 AI functionality for 5x5 Tic-Tac-Toe
 
 This module contains the AI functionality for the tictactoe game.
-It holds the ai_move function, which is a wrapper for the minimax_ab
+It holds the ai_move function, which is a wrapper for the mcts
 workhorse function.
 """
 
@@ -16,60 +16,23 @@ import game
 
 boards = 0
 
-def minimax_ab(board, player_id, depth, maximize, alpha=-inf, beta=-inf) -> (int, int):
+
+class Node:
+    wins = 0
+    games = 0
+    children = []
+
+    def get_children():
+        return self.children
+
+
+def mcts(board, player_id, depth, maximize, alpha=-inf, beta=-inf) -> (int, int):
     """
-    # Minimax_ab
+    # mcts
 
-    This function implements a minimax game tree search with alpha/beta
-    pruning.
+    This function implements a Monte-Carlo Tree Search.
 
-    It returns a tuple of (int, int), which represents the most optimal
-    move and that move's score, which is a sum of the different weights
-    of the boards.
-
-    The board weights are determined by the fastest possible win states.
     """
-    winner = game.check_win(board)
-    if winner == player_id:
-        return None, 10 + depth
-    if winner not in (' ', "draw"):
-        return None, -10 - depth
-    if winner == "draw":
-        return None, 0
-    if depth == 0:
-        return None, None
-
-    global boards
-    boards += 1
-
-    best_move = None
-    value = None
-
-    for i in range(25):
-        if board[i] == ' ':
-            board[i] = player_id
-            _, value = minimax_ab(board,
-                                  'O' if player_id == 'X' else 'X',
-                                  depth - 1,
-                                  False,
-                                  alpha,
-                                  beta)
-            board[i] = ' '
-            if (value is not None
-               and (value > alpha if maximize else value < beta)):
-                if maximize:
-                    alpha = value
-                else:
-                    beta = value
-                if (alpha != -inf and beta != inf
-                   and alpha >= beta if maximize else beta >= alpha):
-                    break
-                best_move = i
-
-    if maximize:
-        return best_move, alpha
-
-    return best_move, beta
 
 
 def ai_move(board, player_id):
@@ -80,7 +43,7 @@ def ai_move(board, player_id):
     global boards
     boards = 0
 
-    move, _ = minimax_ab(board, player_id, 6, True)
+    move, _ = mcts(board, player_id, 6, True)
 
     if move is None:
         move = randint(0, 24)
