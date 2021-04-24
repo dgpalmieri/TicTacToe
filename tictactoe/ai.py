0ff3a78d30eadc63a989c992f8db8394fcec5d9b
Dylan Palmieri <dgpalmieri@gmail.com>
2021-04-23 16:38:01 -0800

    Add initial MCTS functionality

--- tictactoe/ai.py
+++ tictactoe/ai.py
@@ -10,9 +10,9 @@ workhorse function.
 """
 
 
-from math import inf
+from math import inf, sqrt, log
 from random import randint
-from datetime import now
+import time
 import game
 
 boards = 0
@@ -21,53 +21,86 @@ boards = 0
 class Node:
     wins = 0.0
     games = 0.0
-    move = ''
+    move = 0
     board = []
     player_id = ''
     children = []
 
-    def __init__(self, board, player_id):
+    def __init__(self, board, player_id, move):
         self.board = board
         self.player_id = player_id
+        self.move = move
 
     def get_children():
         return self.children
 
 
-def mcts(node, time, start) -> (int, int):
+def mcts(node) -> (int, int):
     """
     # mcts
-
     This function implements a Monte-Carlo Tree Search.
-
     """
+    node.games += 1
+
+    if game.check_win(node.board):
+        node.wins += 1
+        return node.move
+
+    # Selection
+    next_move = -1
+    next_player = 'O' if node.player_id == 'X' else 'X'
+    next_board = copy(board)
+    if node.get_children():
+        max_uct = -inf
+        for child in node.get_children():
+            uct = child.wins/child.games + sqrt(log(node.games) / child.games)
+            if uct > max_uct:
+                max_uct = uct
+                next_move = child.move
+
+    # Expansion and Simulation
+    else:
+        move = random(0, 24)
+        while node.board[move] != ' ':
+            move = random(0, 24)
+
+    next_board[move] = next_player
+    next_node = Node(next_board, next_player, next_move)
+    node.children.append(next_node)
+
+    mcts(next_node)
+
+    # Back-Propogation
     best_score = -inf
-    best_move = 0
-    if now() >= start + time:
+    best_move = -1
+    if time() >= start + time:
         if node.get_children():
-            for child in node.get_children:
+            for child in node.get_children():
                 if child.wins / child.games > best_score:
                     best_score = child.wins / child.games
                     best_move = child.move
-            return (best_move, best_score)
-        else:
-            return None
+        return best_move
 
     return (0,0)
 
 
 def ai_move(board, player_id):
     """
-    ai_move is a wrapper for the minimax_ab function.
+    # ai_move
+    ai_move is a wrapper for the mcts function.
     """
 
     global boards
     boards = 0
 
-    root = Node(board, player_id)
-    move, _ = mcts(root, 15, now())
+    move = -1
+    root = Node(board, player_id, -1)
+    start = time.time()
+    turn_length = 15
+    while time.time() < start + turn_length:
+        move = mcts(root)
 
-    if move is None:
+    if move == -1:
         move = randint(0, 24)
 
     board[move] = player_id
