d556b6a9f479af5495f1c0fcde8b1ec40d55b224
Dylan Palmieri <dgpalmieri@gmail.com>
2021-04-27 12:35:08 -0800

    Fix MCTS: initial working version

--- tictactoe/ai.py
+++ tictactoe/ai.py
@@ -12,7 +12,7 @@ workhorse function.
 
 from math import inf, sqrt, log
 from random import randint
-from copy import copy
+from copy import deepcopy
 import time
 import game
 
@@ -20,62 +20,76 @@ boards = 0
 
 
 class Node:
-    wins = 0.0
-    games = 0.0
-    move = 0
-    board = []
-    player_id = ''
-    children = []
-
     def __init__(self, board, player_id, move):
+        self.wins = 0.0
+        self.games = 0.0
+        self.move = move
         self.board = board
         self.player_id = player_id
-        self.move = move
-
-    def get_children(self):
-        return self.children
+        self.children = []
 
 
-def mcts(node):
+def mcts(node, expanding=False):
     """
     # mcts
     This function implements a Monte-Carlo Tree Search.
     """
     node.games += 1
-
     if game.check_win(node.board) != ' ':
-        node.wins += 1
+        if game.check_win(node.board) == 'X' or game.check_win(node.board) == 'O':
+            node.wins += 1
         return
 
     # Selection
     move = -1
     next_player = 'O' if node.player_id == 'X' else 'X'
-    next_board = copy(node.board)
-    if node.get_children():
+    next_board = deepcopy(node.board)
+    if len(node.children) == 25:
         max_uct = -inf
-        for child in node.get_children():
+        for child in node.children:
             uct = child.wins/child.games + sqrt(log(node.games) / child.games)
             if uct > max_uct:
                 max_uct = uct
                 move = child.move
 
     # Expansion and Simulation
+    elif not expanding:
+        for move_expansion in range(25):
+            if node.board[move_expansion] != ' ':
+                continue
+            next_board = deepcopy(node.board)
+            next_board[move_expansion] = node.player_id
+            next_node = Node(next_board, next_player, move_expansion)
+            is_child = False
+            for child in node.children:
+                if child.board == next_board:
+                    next_node = child
+                    is_child = True
+            if not is_child:
+                node.children.append(next_node)
+            mcts(next_node, True)
     else:
         move = randint(0, 24)
         while node.board[move] != ' ':
             move = randint(0, 24)
-
-    next_board[move] = node.player_id
-    next_node = Node(next_board, next_player, move)
-    node.children.append(next_node)  # appends next_node to node.children and next_node.children
-
-    mcts(next_node)
+        next_board[move] = node.player_id
+        next_node = Node(next_board, next_player, move)
+        is_child = False
+        for child in node.children:
+            if child.board == next_board:
+                next_node = child
+                is_child = True
+        if not is_child:
+            node.children.append(next_node)
+        mcts(next_node, expanding)
 
     # Back-Propagation
-    if node.get_children():
-        for child in node.get_children():
-            node.games += child.games
+    node.wins = 0
+    node.games = 0
+    if node.children:
+        for child in node.children:
             node.wins += child.games - child.wins
+            node.games += child.games
 
 
 def ai_move(board, player_id):
@@ -94,10 +108,10 @@ def ai_move(board, player_id):
     while time.time() < start + turn_length:
         mcts(root)
 
-    best_score = inf
+    best_score = -inf
     print(f"length of children: {len(root.children)}")
     for child in root.children:
-        if child.wins / child.games < best_score:
+        if child.wins / child.games > best_score:
             move = child.move
             best_score = child.wins / child.games
 
